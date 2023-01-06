1526a8bcfed009beb22f6ae7d016e82bf113f9c4
juan <juan.quispem.27@unsch.edu.pe>
2023-01-06 15:02:32 -0500

    modificando ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,27 +1,12 @@
 
 n=int(input("ingrese un número: "))
-def obtener_caracter_hexadecimal(valor):
-    # Lo necesitamos como cadena
-    valor = str(valor)
-    equivalencias = {
-        "10": "a",
-        "11": "b",
-        "12": "c",
-        "13": "d",
-        "14": "e",
-        "15": "f",
-    }
-    if valor in equivalencias:
-        return equivalencias[valor]
-    else:
-        return valor
-
 def decimal_a_hexadecimal(decimal):
     hexadecimal = ""
+    suma=0
     while decimal > 0:
         residuo = decimal % 16
-        verdadero_caracter = obtener_caracter_hexadecimal(residuo)
-        hexadecimal = verdadero_caracter + hexadecimal
+        suma +=residuo 
+        hexadecimal = residuo + hexadecimal
         decimal = int(decimal / 16)
     return hexadecimal
 print(decimal_a_hexadecimal(n))
